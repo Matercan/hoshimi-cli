9301a7770feaa06b4f95e66af17545e2f9749aef
matercan <matercan@proton.me>
2025-10-01 18:18:14 +0200

    Update Program: commands + specified packages sourcing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 
 #include "files.hpp"
-#include "utils/headers.hpp"
 #include "version.h"
+#include <vector>
 
 namespace fs = std::filesystem;
 std::vector<std::string> packages;
@@ -12,20 +12,15 @@ struct Flag {
   std::vector<std::string> args;
   std::string description;
 
-  Flag(bool on, const std::vector<std::string> &argss,
-       const std::string &desc = "")
-      : present(on), args(argss), description(desc) {}
+  Flag(bool on, const std::vector<std::string> &argss, const std::string &desc = "") : present(on), args(argss), description(desc) {}
 
   // Alternative constructor for initializer list
-  Flag(bool on, std::initializer_list<std::string> argss,
-       const std::string &desc = "")
-      : present(on), args(argss), description(desc) {}
+  Flag(bool on, std::initializer_list<std::string> argss, const std::string &desc = "") : present(on), args(argss), description(desc) {}
 
   Flag() : present(false), description("") {}
 };
 
-void print_help(const std::string &program_name,
-                const std::vector<Flag> &config) {
+void print_help(const std::string &program_name, const std::vector<Flag> &config) {
   std::cout << "Hoshimi - Hyprland Dotfiles Manager\n";
   std::cout << "===================================\n\n";
 
@@ -39,19 +34,16 @@ void print_help(const std::string &program_name,
   std::cout << "    arch-install  Install all the packages neccessary for this "
                "shell using paru\n";
   std::cout << "    version       Get version information of hoshimi\n";
-  std::cout
-      << "    update        Update dotfiles to the most recent master commit\n";
+  std::cout << "    update        Update dotfiles to the most recent master commit\n";
   std::cout << "    config        Get or set the config options within your "
                "configuration\n";
   std::cout << "    source        Source the current configuration, updating "
                "the modifiable dotfiles \n";
-  std::cout
-      << "    restart       (re)start the shell and reload terminals. \n\n";
+  std::cout << "    restart       (re)start the shell and reload terminals. \n\n";
 
   std::cout << "OPTIONS:\n";
 
-  std::cout
-      << "    -h, --help                              Show this help message\n";
+  std::cout << "    -h, --help                              Show this help message\n";
   std::cout << "    -v, --verbose                           Enable verbose "
                "output (show detailed operations)\n";
   std::cout << "    -f, --force                             Force overwrite "
@@ -69,8 +61,7 @@ void print_help(const std::string &program_name,
   std::cout << "    " + program_name + " install -p hypr,fastfetch -v\n";
   std::cout << "    " + program_name + " source -p quickshell\n";
   std::cout << "    " + program_name + " arch-install\n";
-  std::cout << "    " + program_name +
-                   " install -np hypr --no-secondary-commands\n\n";
+  std::cout << "    " + program_name + " install -np hypr --no-secondary-commands\n\n";
 
   std::cout << "Subcommands have their own help" << std::endl;
 }
@@ -81,23 +72,17 @@ int archInstall(std::vector<Flag> &config, bool &retFlag);
 
 void getPackageInfo(int argc, std::vector<Flag> &config, char *argv[]);
 
-void getConfigArg(int argc, char *argv[], bool &setB, std::string &configArg,
-                  std::vector<std::string> &vec);
+void getConfigArg(int argc, char *argv[], bool &setB, std::string &configArg, std::vector<std::string> &vec);
 
-void sourceConfig();
+void sourceConfig(std::vector<Flag> config);
 
 int main(int argc, char *argv[]) {
-  std::vector<Flag> config = {
-      Flag(false, {"-v", "--verbose"},
-           "Enable verbose output (show detailed operations)"),
-      Flag(false, {"-f", "--force"},
-           "Force overwrite existing files without backup"),
-      Flag(false, {"-h", "--help"}, "Show this help message"),
-      Flag(false, {"-p", "--packages"}, "Packages that you want to install"),
-      Flag(false, {"-np", "--not-packages"},
-           "Not-Packages that you want to install"),
-      Flag(false, {"--no-secondary-commands"},
-           "Don't show the secondnary commands")};
+  std::vector<Flag> config = {Flag(false, {"-v", "--verbose"}, "Enable verbose output (show detailed operations)"),
+                              Flag(false, {"-f", "--force"}, "Force overwrite existing files without backup"),
+                              Flag(false, {"-h", "--help"}, "Show this help message"),
+                              Flag(false, {"-p", "--packages"}, "Packages that you want to install"),
+                              Flag(false, {"-np", "--not-packages"}, "Not-Packages that you want to install"),
+                              Flag(false, {"--no-secondary-commands"}, "Don't show the secondnary commands")};
 
   // Check if we have enough arguments
   if (argc < 2) {
@@ -117,24 +102,19 @@ int main(int argc, char *argv[]) {
       std::cout << "Dotfiles modifiable by Hoshimi config will be copied "
                    "instead of symlinked."
                 << std::endl;
-      std::cout << "The dotfiles' source is located in "
-                << filesManager.getdotfilesDirectory() << std::endl;
+      std::cout << "The dotfiles' source is located in " << filesManager.getdotfilesDirectory() << std::endl;
       std::cout << "Unless running in force mode, the existing files will be "
                    "backed up to "
-                << filesManager.getdotfilesDirectory() / ".backup/"
-                << " before being replaced." << std::endl;
-      std::cout << "Use '" << argv[0]
-                << " help' to see all available commands and options."
-                << std::endl;
+                << filesManager.getdotfilesDirectory() / ".backup/" << " before being replaced." << std::endl;
+      std::cout << "Use '" << argv[0] << " help' to see all available commands and options." << std::endl;
       return 0;
     }
 
-    if (filesManager.install_dotfiles(packages, notPackages, config[0].present,
-                                      config[3].present) != 0)
+    if (filesManager.install_dotfiles(packages, notPackages, config[0].present, config[3].present) != 0)
       return 1;
 
     if (!config[5].present)
-      sourceConfig();
+      sourceConfig(config);
 
     std::cout << std::endl << "Hoshimi Dotfiles installed ";
     if (!config[1].present)
@@ -148,24 +128,18 @@ int main(int argc, char *argv[]) {
                 << " update updates hoshimi dotfiles to the most recent commit "
                    "on master branch."
                 << std::endl;
-      std::cout << "The dotfiles' source is located in "
-                << filesManager.getdotfilesDirectory() << std::endl;
-      std::cout << "Use '" << argv[0]
-                << " help' to see all available commands and options."
-                << std::endl;
+      std::cout << "The dotfiles' source is located in " << filesManager.getdotfilesDirectory() << std::endl;
+      std::cout << "Use '" << argv[0] << " help' to see all available commands and options." << std::endl;
       return 0;
     }
 
-    std::filesystem::path HOSHIMI_HOME =
-        filesManager.getdotfilesDirectory().parent_path();
+    std::filesystem::path HOSHIMI_HOME = filesManager.getdotfilesDirectory().parent_path();
 
-    const std::string UPDATE_COMMAND =
-        "cd " + HOSHIMI_HOME.string() + " && git pull";
+    const std::string UPDATE_COMMAND = "cd " + HOSHIMI_HOME.string() + " && git pull";
     if (system(UPDATE_COMMAND.c_str()) == 0) {
       return 0;
     } else {
-      std::cout << "Hoshimi failed to update. Check if the directory "
-                << HOSHIMI_HOME.string() << " exists or not";
+      std::cout << "Hoshimi failed to update. Check if the directory " << HOSHIMI_HOME.string() << " exists or not";
       return 2;
     }
   } else if (command == "source") {
@@ -177,38 +151,10 @@ int main(int argc, char *argv[]) {
       std::cout << "You can specify which packages to source with the "
                    "-p/--packages and -np/--not-packages flags."
                 << std::endl;
-      std::cout << "Use '" << argv[0]
-                << " help' to see all available commands and options."
-                << std::endl;
+      std::cout << "Use '" << argv[0] << " help' to see all available commands and options." << std::endl;
       return 0;
     }
-
-    if (config[3].present) {
-      for (size_t i = 0; i < packages.size(); ++i) {
-        if (packages[i] == "ghostty") {
-          GhosttyWriter gs;
-          gs.writeConfig();
-        } else if (packages[i] == "quickshell") {
-          QuickshellWriter qs;
-          qs.writeColors();
-        } else if (packages[i] == "alacritty") {
-          AlacrittyWriter as;
-          as.writeConfig();
-        } else if (packages[i] == "foot") {
-          FootWriter fs;
-          fs.writeConfig();
-        }
-      }
-    } else {
-      sourceConfig();
-    }
-    auto shellConfig = ShellHandler().getConfig();
-    for (int i = 0; shellConfig.commands && shellConfig.commands[i]; ++i) {
-      if (system(shellConfig.commands[i]) != 0) {
-        HERR("main") << "Failed to run command: " << shellConfig.commands[i]
-                     << std::endl;
-      }
-    }
+    sourceConfig(config);
 
   } else if (command == "config") {
     if (config[2].present) {
@@ -216,14 +162,10 @@ int main(int argc, char *argv[]) {
                 << " config gets or sets the config options within your "
                    "configuration."
                 << std::endl;
-      std::cout << "Usage: " << argv[0]
-                << " config <key1> <key2> ... get/set <value>" << std::endl;
-      std::cout
-          << "For example, to set the wallpaper directory: " << argv[0]
-          << " config globals wallpaperDirectory set ~/Pictures/Wallpapers"
-          << std::endl;
-      std::cout << "To get the current wallpaper directory: " << argv[0]
-                << " config globals wallpaperDirectory get" << std::endl;
+      std::cout << "Usage: " << argv[0] << " config <key1> <key2> ... get/set <value>" << std::endl;
+      std::cout << "For example, to set the wallpaper directory: " << argv[0] << " config globals wallpaperDirectory set ~/Pictures/Wallpapers"
+                << std::endl;
+      std::cout << "To get the current wallpaper directory: " << argv[0] << " config globals wallpaperDirectory get" << std::endl;
       return 0;
     }
 
@@ -235,8 +177,7 @@ int main(int argc, char *argv[]) {
 
     if (vec.empty() || configArg.empty()) {
       std::cerr << "Error: Invalid config command format" << std::endl;
-      std::cout << "Usage: " << argv[0]
-                << " config <key1> <key2> ... get/set <value>" << std::endl;
+      std::cout << "Usage: " << argv[0] << " config <key1> <key2> ... get/set <value>" << std::endl;
       return 1;
     }
 
@@ -247,14 +188,11 @@ int main(int argc, char *argv[]) {
         std::cout << "Write config options in a list and then set with the "
                      "value you want to set it to"
                   << std::endl;
-        std::cout
-            << "For example: " << argv[0]
-            << " config globals wallpaperDirectory set ~/Pictures/Wallpapers"
-            << std::endl;
+        std::cout << "For example: " << argv[0] << " config globals wallpaperDirectory set ~/Pictures/Wallpapers" << std::endl;
         return 1;
       } else {
         if (!config[5].present)
-          return sourceConfig(), 0;
+          return sourceConfig(config), 0;
       }
     } else
       std::cout << js.getJson(vec) << std::endl;
@@ -266,9 +204,7 @@ int main(int argc, char *argv[]) {
                 << " arch-install installs all the packages neccessary for "
                    "this shell using paru."
                 << std::endl;
-      std::cout << "Use '" << argv[0]
-                << " help' to see all available commands and options."
-                << std::endl;
+      std::cout << "Use '" << argv[0] << " help' to see all available commands and options." << std::endl;
       return 0;
     }
 
@@ -279,12 +215,8 @@ int main(int argc, char *argv[]) {
 
   } else if (command == "restart") {
     if (config[2].present) {
-      std::cout << argv[0]
-                << " restart (re)starts the shell and reloads terminals."
-                << std::endl;
-      std::cout << "Use '" << argv[0]
-                << " help' to see all available commands and options."
-                << std::endl;
+      std::cout << argv[0] << " restart (re)starts the shell and reloads terminals." << std::endl;
+      std::cout << "Use '" << argv[0] << " help' to see all available commands and options." << std::endl;
       return 0;
     }
 
@@ -298,31 +230,54 @@ int main(int argc, char *argv[]) {
   } else if (command == "help" || config[2].present) {
     print_help(argv[0], config);
   } else {
-    HERR("main") << "Unknown command: " << command << "Use '" << argv[0]
-                 << " help' to see available commands." << std::endl;
+    HERR("main") << "Unknown command: " << command << "Use '" << argv[0] << " help' to see available commands." << std::endl;
     return 1;
   }
 }
 
-void sourceConfig() {
-  GhosttyWriter *gs = new GhosttyWriter();
-  gs->writeConfig();
-  delete gs;
-  AlacrittyWriter *as = new AlacrittyWriter();
-  as->writeConfig();
-  delete as;
-  FootWriter *fs = new FootWriter();
-  fs->writeConfig();
-  delete fs;
+void sourceConfig(std::vector<Flag> config) {
+  if (config[3].present) {
+    for (size_t i = 0; i < packages.size(); ++i) {
+      if (packages[i] == "ghostty") {
+        GhosttyWriter gs;
+        gs.writeConfig();
+      } else if (packages[i] == "quickshell") {
+        QuickshellWriter qs;
+        qs.writeColors();
+      } else if (packages[i] == "alacritty") {
+        AlacrittyWriter as;
+        as.writeConfig();
+      } else if (packages[i] == "foot") {
+        FootWriter fs;
+        fs.writeConfig();
+      }
+    }
+  } else {
+    GhosttyWriter *gs = new GhosttyWriter();
+    gs->writeConfig();
+    delete gs;
+    AlacrittyWriter *as = new AlacrittyWriter();
+    as->writeConfig();
+    delete as;
+    FootWriter *fs = new FootWriter();
+    fs->writeConfig();
+    delete fs;
 
-  QuickshellWriter *qs = new QuickshellWriter();
-  qs->writeColors();
-  qs->writeShell();
-  delete qs;
+    QuickshellWriter *qs = new QuickshellWriter();
+    qs->writeColors();
+    qs->writeShell();
+    delete qs;
+  }
+
+  auto shellConfig = ShellHandler().getConfig();
+  for (int i = 0; shellConfig.commands && shellConfig.commands[i]; ++i) {
+    if (system(shellConfig.commands[i]) != 0) {
+      HERR("main") << "Failed to run command: " << shellConfig.commands[i] << std::endl;
+    }
+  }
 }
 
-void getConfigArg(int argc, char *argv[], bool &setB, std::string &configArg,
-                  std::vector<std::string> &vec) {
+void getConfigArg(int argc, char *argv[], bool &setB, std::string &configArg, std::vector<std::string> &vec) {
   // Process arguments until we find "set"
   for (int i = 2; i < argc; ++i) {
     if (strcmp(argv[i], "set") == 0) {
@@ -359,11 +314,9 @@ void getPackageInfo(int argc, std::vector<Flag> &config, char *argv[]) {
     }
 
     if (i == packagesArgument) {
-      boost::split(packages, argv[i], boost::is_any_of(","),
-                   boost::token_compress_on);
+      boost::split(packages, argv[i], boost::is_any_of(","), boost::token_compress_on);
     } else if (i == noPackagesArgument) {
-      boost::split(notPackages, argv[i], boost::is_any_of(","),
-                   boost::token_compress_on);
+      boost::split(notPackages, argv[i], boost::is_any_of(","), boost::token_compress_on);
     }
   }
 }
@@ -418,7 +371,7 @@ int archInstall(std::vector<Flag> &config, bool &retFlag) {
 
 void restart(std::vector<Flag> &config, char *argv[]) {
   if (!config[5].present)
-    sourceConfig();
+    sourceConfig(config);
 
   GhosttyWriter *gs = new GhosttyWriter();
   gs->reloadGhostty();
